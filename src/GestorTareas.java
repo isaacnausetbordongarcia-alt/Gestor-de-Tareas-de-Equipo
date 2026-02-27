ba1b9e943632445c875c46ae1ab5c870e4956a3f
isaacnausetbordongarcia-alt <isaacnausetbordongarcia@alumno.ieselrincon.es>
2026-02-27 20:37:55 +0000

    feat anadirTareas

--- src/GestorTareas.java
+++ src/GestorTareas.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class GestorTareas {
+
+    private ArrayList<Tarea> tareas;
+
+    public GestorTareas() {
+        tareas = new ArrayList<>();
+    }
+
+    public void anadirTarea(String descripcion, boolean finalizado) {
+        Tarea nuevaTarea = new Tarea(descripcion,finalizado);
+        tareas.add(nuevaTarea);
+        System.out.println("Tarea añadida correctamente.");
+    }
 }
