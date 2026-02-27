42e970ec8b397f8a189654980acc63a0a1e34a05
jonathannicolaslarazepeda <jonathannicolaslarazepeda@alumno.ieselrincon.es>
2026-02-27 20:37:35 +0000

    feat mM©todo Mostrar Tareas

--- src/GestorTareas.java
+++ src/GestorTareas.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class GestorTareas {
+
+    ArrayList<Tarea> listaTareas = new ArrayList<>();
+
+    /**
+     * Este el el método definitivo para leer ArrayList de Listas de Tareas, cuya misión principal es leer el contenido de la lista de tareas.
+     */
+
+    void mostrarTareas(){
+        for (Tarea p : listaTareas) {
+            System.out.println("Nombre de la Tarea: " + p.getNombre() + ", estado " + p.isCompletada());
+        }
+    }
+
 }
